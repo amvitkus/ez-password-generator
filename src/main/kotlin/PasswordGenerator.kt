08742e0cea7e3992043f44b6907e4f405c982aa9
Aras <amvitkus@gmail.com>
2022-08-19 14:10:22 -0400

    Refactoring and start of testing.

--- src/main/kotlin/PasswordGenerator.kt
+++ src/main/kotlin/PasswordGenerator.kt
@@ -1,27 +1,36 @@
-class PasswordGenerator {
+import java.util.Scanner
 
-    fun generator(){
+fun main(){
+    //PasswordGenerator()
 
-            var password: String = ""
+    var password: String = ""
+    //Alphabet with no O/o letters
+    val letters: Array<Char> = arrayOf('A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z')
+    val symbols: Array<Char> = arrayOf('!', '@', '#', '$', ';', '%', '&', '?', '*', '=', '+', '<', '>')
+    var running = true
+    var runAgain = Scanner(System.`in`)
 
-            val letters: Array<Char> = arrayOf('A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z')
 
-            val symbols: Array<Char> = arrayOf('~', '`', '!', '@', '#', '№', '$', ';', '%', '^', '&', '?', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', '/', ':', ',', '<', '.', '>')
+    //do {
+        print("Please enter a whole number greater than 0 for your password length: ")
+        val passwordLength: Int = readLine()!!.toInt()
 
-            val passwordLength: Int = readLine()!!.toInt()
+        for (i in 1..passwordLength) {
 
-            for(i in 1..passwordLength) {
+            val number: Int = (0..2).random()
 
-                val number: Int = (0..2).random()
-
-                when (number) {
-                    0 -> password += letters[(0..51).random()]
-                    1 -> password += symbols[(0..29).random()]
-                    2 -> password += (0..9).random().toString()
-                }
+            when (number) {
+                0 -> password += letters[(0..49).random()]
+                1 -> password += symbols[(0..12).random()]
+                2 -> password += (0..9).random().toString()
             }
+        }
+    //} while (running)
 
             println("Password: $password")
-    }
+//            println("Generate new password?(y/n) ")
+//            var runAgain = readLine()!!.toString()
+//
+//            if (runAgain == "y")
 
 }
---
+++ src/test/kotlin/PasswordTest.kt
@@ -0,0 +1,14 @@
+import org.junit.Test
+
+class PasswordGenerator {
+
+}
+
+class PasswordTest {
+
+    @Test
+    fun lengthNine() {
+        val passwordGenerator = PasswordGenerator()
+        //val result = passwordGenerator.
+    }
+}
