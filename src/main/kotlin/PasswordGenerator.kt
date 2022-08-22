4d5e46e89c21c1a62fbde55e2e09aaed5855a17c
Aras <amvitkus@gmail.com>
2022-08-21 21:33:08 -0400

    User is now asked to run program again or gracefully exit.

--- src/main/kotlin/PasswordGenerator.kt
+++ src/main/kotlin/PasswordGenerator.kt
@@ -1,36 +1,94 @@
-import java.util.Scanner
 
-fun main(){
-    //PasswordGenerator()
+fun main() {
 
-    var password: String = ""
+    var password = ""
     //Alphabet with no O/o letters
-    val letters: Array<Char> = arrayOf('A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z')
+    val letters: Array<Char> = arrayOf(
+        'A',
+        'a',
+        'B',
+        'b',
+        'C',
+        'c',
+        'D',
+        'd',
+        'E',
+        'e',
+        'F',
+        'f',
+        'G',
+        'g',
+        'H',
+        'h',
+        'I',
+        'i',
+        'J',
+        'j',
+        'K',
+        'k',
+        'L',
+        'l',
+        'M',
+        'm',
+        'N',
+        'n',
+        'P',
+        'p',
+        'Q',
+        'q',
+        'R',
+        'r',
+        'S',
+        's',
+        'T',
+        't',
+        'U',
+        'u',
+        'V',
+        'v',
+        'W',
+        'w',
+        'X',
+        'x',
+        'Y',
+        'y',
+        'Z',
+        'z'
+    )
     val symbols: Array<Char> = arrayOf('!', '@', '#', '$', ';', '%', '&', '?', '*', '=', '+', '<', '>')
     var running = true
-    var runAgain = Scanner(System.`in`)
+ //   var runAgain = Scanner(System.`in`)
 
 
-    //do {
-        print("Please enter a whole number greater than 0 for your password length: ")
-        val passwordLength: Int = readLine()!!.toInt()
+    do {
+    print("Please enter a whole number greater than 0 for your password length: ")
+    val passwordLength: Int = readLine()!!.toInt()
 
-        for (i in 1..passwordLength) {
+    for (i in 1..passwordLength) {
 
-            val number: Int = (0..2).random()
+        val number: Int = (0..2).random()
 
-            when (number) {
-                0 -> password += letters[(0..49).random()]
-                1 -> password += symbols[(0..12).random()]
-                2 -> password += (0..9).random().toString()
-            }
+        when (number) {
+            0 -> password += letters[(0..49).random()]
+            1 -> password += symbols[(0..12).random()]
+            2 -> password += (0..9).random().toString()
         }
+    }
     //} while (running)
 
-            println("Password: $password")
-//            println("Generate new password?(y/n) ")
-//            var runAgain = readLine()!!.toString()
+    println("Password: $password")
+
+            println("Generate new password?(y/n) ")
+            val runAgain = readLine()!!.toString()
 //
-//            if (runAgain == "y")
+            if (runAgain == "n") {
+                println("Exiting program, goodbye!")
+                running = false
+            } else {
+                password = ""
+            }
+
+} while (running)
+
 
 }
